bce17e332067b5cdd37bb0658408e4f035d6a6c5
Marcin Zawalski <zawalskimarcin@gmail.com>
2026-06-13 20:15:41 +0200

    fix default export icc

--- negpy/domain/models.py
+++ negpy/domain/models.py
@@ -83,7 +83,7 @@ class ExportConfig:
 
     export_path: str = field(default_factory=lambda: os.path.join(paths.get_default_user_dir(), "export"))
     export_fmt: str = ExportFormat.JPEG
-    export_color_space: str = ColorSpace.SAME_AS_SOURCE.value
+    export_color_space: str = ColorSpace.ADOBE_RGB.value
     paper_aspect_ratio: str = AspectRatio.ORIGINAL
     export_print_size: float = 30.0
     export_dpi: int = 300
